85f47d7cd7cf3f955d328c07f3c4f8ea481cb524
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-05-02 11:55:18 +0700

    1717A - Madoka and Strange Thoughts  - Update status.

--- 1717A_MadokaAndStrangeThoughts.cpp
+++ 1717A_MadokaAndStrangeThoughts.cpp
@@ -1,3 +1,5 @@
+//* 202374540	Apr/16/2023 20:48UTC+7	Minh4893IT	A - Madoka and Strange Thoughts	GNU C++17	Accepted	0 ms	0 KB
+
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long int;

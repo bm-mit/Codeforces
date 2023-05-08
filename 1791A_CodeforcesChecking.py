1a3f47adfcee4148aa84a0ac916c691882aec0e0
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-05-08 12:43:52 +0700

    1791A - Codeforces Checking  - Update status.  - Rename file.

---
+++ 1791A_CodeforcesChecking.py
@@ -0,0 +1,4 @@
+#   200666775	Apr/04/2023 17:24UTC+7	Minh4893IT	A - Codeforces Checking	PyPy 3	Accepted	62 ms	0 KB
+
+for i in range(int(input())):
+    print("yes" if input() in "codefrcs" else "no")
--- CodeforcesChecking.py
+++
@@ -1,2 +0,0 @@
-for i in range(int(input())):
-    print("yes" if input() in "codefrcs" else "no")

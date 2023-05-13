711560c111fdac117699932cfdfaca4b463b3c23
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-05-13 13:54:02 +0700

    Update .template.cpp  - Remove ll macro.  - Remove vec macro.  - Remove pb and popb macros.  - Remove all, rall macros,  - Remove sint type.  - Remove OFILE argument template.

--- .template.cpp
+++ .template.cpp
@@ -1,32 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using sint = signed int;
-#define ll long long
-#define ull unsigned ll
-#define int ll
+#define int long long
 #define ss stringstream
 #define uset unordered_set
 #define umap unordered_map
 #define mmap multimap
 #define mset multiset
-#define vec vector
 #define pq priority_queue
-#define pb push_back
-#define popb pop_back
+#define endl "\n"
 
 #define LLMAX INT64_MAX
-
 #define GET(var) getline(cin, var)
-
-#define endl "\n"
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-
 #define EACH(x, a) for (auto &x : a)
 #define IF(cond, t, f) (cond ? t : f)
-template <class T>
-void OFILE(T finp, T fout)
+void OFILE(const char * finp, const char * fout)
 {
     freopen(finp, "r", stdin), freopen(fout, "w", stdout);
 }
@@ -36,7 +24,7 @@ void solve()
     // Solution
 }
 
-sint main()
+signed main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie();

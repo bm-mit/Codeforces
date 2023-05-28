6b37da3baedb16727517fc7b08a18f562762960f
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-05-28 13:27:56 +0700

    1837A - Grasshopper on a Line

---
+++ 1837A_GrasshopperOnALine.cpp
@@ -0,0 +1,44 @@
+//* 207535485	May/28/2023 13:27UTC+7	Minh4893IT	A - Grasshopper on a Line	GNU C++17	Accepted	0 ms	0 KB
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ss stringstream
+#define uset unordered_set
+#define umap unordered_map
+#define mmap multimap
+#define mset multiset
+#define pq priority_queue
+#define endl "\n"
+
+#define LLMAX INT64_MAX
+#define GET(var) getline(cin, var)
+#define EACH(x, a) for (auto &x : a)
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie()
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    if (n % k)
+        cout << 1 << endl << n << endl;
+    else
+        cout << 2 << endl << n + 1 << ' ' << -1 << endl;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}

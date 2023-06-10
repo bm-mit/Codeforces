3f9e460bace14f6a09aa16bebc6296c8eb3e8c33
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-06-10 16:49:47 +0700

    1840A - Cipher Shifer

---
+++ 1840A_CipherShifer.cpp
@@ -0,0 +1,49 @@
+//* 209178937	Jun/10/2023 16:47UTC+7	Minh4893IT	A - Cipher Shifer	GNU C++17	Accepted	15 ms	100 KB
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ss stringstream
+#define uset unordered_set
+#define umap unordered_map
+#define mmap multimap
+#define mset multiset
+#define pq priority_queue
+#define endl "\n"
+
+#define LLMAX INT64_MAX
+#define GET(var) getline(cin, var)
+#define EACH(x, a) for (auto &x : a)
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie()
+
+void solve()
+{
+    string s;
+    cin >> s >> s;
+
+    string result = {s[0]};
+
+    for (int i = 1; i < s.size() - 1; ++i)
+    {
+        if (s[i] == result.back())
+            result += s[++i];
+    }
+
+    cout << result << endl;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}

a1d5a7ffe61104df0d4bddc1c7077a70b01900c9
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-06-11 21:00:04 +0700

    1748A - The Ultimate Square

---
+++ 1748A_TheUltimateSquare.cpp
@@ -0,0 +1,40 @@
+//* 209290005	Jun/11/2023 20:59UTC+7	Minh4893IT	A - The Ultimate Square	GNU C++17	Accepted	31 ms	0 KB
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ss stringstream
+#define uset unordered_set
+#define umap unordered_map
+#define mmap multimap
+#define mset multiset
+#define pq priority_queue
+#define endl "\n"
+
+#define LLMAX INT64_MAX
+#define GET(var) getline(cin, var)
+#define EACH(x, a) for (auto &x : a)
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie()
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << n / 2 + n % 2 << endl;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}

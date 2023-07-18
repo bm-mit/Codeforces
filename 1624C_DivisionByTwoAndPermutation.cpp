fdb8760c76fcd3d1694649ae3cd1dbcb45f67203
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-07-18 10:18:20 +0700

    1624C - Division by Two and Permutation

---
+++ 1624C_DivisionByTwoAndPermutation.cpp
@@ -0,0 +1,53 @@
+//* 214285324	Jul/18/2023 10:08UTC+7	Minh4893IT	C - Division by Two and Permutation	GNU C++14	Accepted	249 ms	200 KB
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ss stringstream
+#define uset unordered_set
+#define umap unordered_map
+#define mmap multimap
+#define mset multiset
+#define pq priority_queue
+#define endl "\n"
+
+#define LLMAX INT64_MAX
+#define GET(var) getline(cin, var)
+#define EACH(x, a) for (auto &x : a)
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie()
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; ++i)
+        cin >> arr[i];
+
+    map<int, bool> appear;
+    for (auto elem : arr)
+    {
+        while (elem && (elem > n || appear[elem]))
+            elem >>= 1;
+        if (elem)
+            appear[elem] = true;
+    }
+    cout << IF(appear.size() == n, "YES", "NO") << endl;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}

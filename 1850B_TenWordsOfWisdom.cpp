369d9c56ec6e24c022acb9914c4fd958a32ef735
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-07-24 20:52:50 +0700

    1850B - Ten Words of Wisdom

---
+++ 1850B_TenWordsOfWisdom.cpp
@@ -0,0 +1,51 @@
+//* 214784268	Jul/21/2023 21:43UTC+7	Minh4893IT	B - Ten Words of Wisdom	GNU C++14	Accepted	15 ms	0 KB
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ss stringstream
+#define uset unordered_set
+#define umap unordered_map
+#define mmap multimap
+#define mset multiset
+#define pq priority_queue
+#define endl "\n"
+
+#define LLMAX INT64_MAX
+#define GET(var) getline(cin, var)
+#define EACH(x, a) for (auto &x : a)
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie()
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> arr;
+    for (int i = 0; i < n; ++i)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a <= 10)
+            arr.push_back({b, i + 1});
+    }
+
+    sort(arr.rbegin(), arr.rend());
+    cout << arr.front().second << endl;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}

1a8bf42b29243b0e8ce4e14ced7e9b804851bf5d
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-07-24 20:50:31 +0700

    1853A - Desorting

---
+++ 1853A_Desorting.cpp
@@ -0,0 +1,54 @@
+//* 215193683	Jul/23/2023 21:39UTC+7	Minh4893IT	A - Desorting	GNU C++14	Accepted	15 ms	100 KB
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ss stringstream
+#define uset unordered_set
+#define umap unordered_map
+#define mmap multimap
+#define mset multiset
+#define pq priority_queue
+#define endl "\n"
+
+#define LLMAX INT64_MAX
+#define GET(var) getline(cin, var)
+#define EACH(x, a) for (auto &x : a)
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie()
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i)
+        cin >> arr[i];
+    
+    if (!is_sorted(arr.begin(), arr.end()))
+    {
+        cout << 0 << endl;
+        return;
+    }
+
+    int result = LLMAX;
+    for (int i = 1; i < n; ++i)
+        result = min(result, arr[i] - arr[i - 1]);
+    cout << result / 2 + 1 << endl;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
